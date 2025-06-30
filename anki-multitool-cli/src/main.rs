01e511b6244f4fe00830667540e6e0848cc4e58c
Bohdan Sokolovskyi <sokol.chemist@gmail.com>
2025-06-30 16:01:40 +0300

    Revert "resolve merge conflicts"
    This reverts commit 29daa6dfcf7d3803c389045b7f9f5b5f3b6a705b, reversing
    changes made to eb199dee76339a33fbe7af82a4d6685670f2e54e.

--- anki-multitool-cli/src/main.rs
+++ anki-multitool-cli/src/main.rs
@@ -1,17 +1,3 @@
-mod cli;
-
-use clap::Parser;
-use cli::{
-    Cli,
-    Command
-};
-
 fn main() {
-    let cli = Cli::parse();
-    
-    match cli.command {
-        Command::Version => {
-            println!("Anki-multitool version: {}", env!("CARGO_PKG_VERSION"));
-        }
-    }
+    println!("Hello, world!");
 }
